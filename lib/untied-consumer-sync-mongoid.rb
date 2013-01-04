a49575ca361f08bc5694f7c44cb6d8a81d1097ad
Juliana Lucena <julianalucenaa@gmail.com>
2013-01-04 11:34:36 -0300

    Sets mongoid as backend,

--- lib/untied-consumer-sync-mongoid.rb
+++ lib/untied-consumer-sync-mongoid.rb
@@ -1,12 +1,3 @@
-require "untied-consumer-sync-mongoid/version"
-require "untied-consumer-sync-mongoid/backend/mongoid"
 require "untied-consumer-sync"
 
-module Untied
-  module Consumer
-    module Sync
-      module Mongoid
-      end
-    end
-  end
-end
+Untied::Consumer::Sync.backend = :mongoid
