fc1cc01a29e96619393eb9ee85f8cb7ee6aa8809
Juliana Lucena <julianalucenaa@gmail.com>
2013-01-04 11:33:42 -0300

    Fixes test migrations.

--- spec/support/setup_models.rb
+++ spec/support/setup_models.rb
@@ -2,7 +2,7 @@ require 'mongoid'
 
 module SetupModels
   Mongoid.configure do |config|
-    config.connect_to("dl_spec")
+    config.connect_to("untied_consumer_mongoid_spec")
   end
 
   # Models
@@ -13,6 +13,8 @@ module SetupModels
     field :my_id, :type => String
     field :login, :type => String
     field :name, :type => String
-    field :zombie, :type => String, :default => true
+    field :zombie, :type => Boolean, :default => true
+
+    attr_accessible :my_id, :login, :name
   end
 end
